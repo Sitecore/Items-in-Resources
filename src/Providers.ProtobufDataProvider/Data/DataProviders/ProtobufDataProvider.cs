86d5a4055f96b441990093d1b3cca8c9093f43ba
AlenPelin <AlenPelin@gmail.com>
2016-11-17 18:17:46 +1000

    Fix missing languages issue

--- src/Providers.ProtobufDataProvider/Data/DataProviders/ProtobufDataProvider.cs
+++ src/Providers.ProtobufDataProvider/Data/DataProviders/ProtobufDataProvider.cs
@@ -211,7 +211,7 @@
     public override IEnumerable<Language> GetLanguages()
     {
       var languageList = this.DataSet.Children.TryGetValue(ItemIDs.LanguagesRootId);
-      if (languageList != null)
+      if (languageList == null)
       {
         return new Language[0];
       }
