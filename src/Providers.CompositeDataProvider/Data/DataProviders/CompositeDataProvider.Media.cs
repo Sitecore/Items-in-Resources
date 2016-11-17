41b6c89bad9c404d68021880c9784d50cfc2ffd4
AlenPelin <AlenPelin@gmail.com>
2016-11-17 20:25:53 +1000

    Remove not supported read-only provider media calls

--- src/Providers.CompositeDataProvider/Data/DataProviders/CompositeDataProvider.Media.cs
+++ src/Providers.CompositeDataProvider/Data/DataProviders/CompositeDataProvider.Media.cs
@@ -10,25 +10,22 @@
     /* Media part of DataProvider */
 
     public override bool BlobStreamExists(Guid blobId, CallContext context)
-    {
-      return HeadProvider.BlobStreamExists(blobId, context) ||
-        ReadOnlyProviders.Any(x => x.BlobStreamExists(blobId, context));
+    {          
+      return HeadProvider.BlobStreamExists(blobId, context);
     }
 
     public override Stream GetBlobStream(Guid blobId, CallContext context)
-    {
-      return HeadProvider.GetBlobStream(blobId, context) ??
-        ReadOnlyProviders.FirstNotNull(x => x.GetBlobStream(blobId, context));
+    {     
+      return HeadProvider.GetBlobStream(blobId, context);
     }
 
     public override bool SetBlobStream(Stream stream, Guid blobId, CallContext context)
-    {
-      return HeadProvider.SetBlobStream(stream, blobId, context) ||
-        ReadOnlyProviders.Any(x => x.SetBlobStream(stream, blobId, context));
+    {       
+      return HeadProvider.SetBlobStream(stream, blobId, context);
     }
 
     public override bool RemoveBlobStream(Guid blobId, CallContext context)
-    {
+    {      
       return HeadProvider.RemoveBlobStream(blobId, context);
     }
   }
