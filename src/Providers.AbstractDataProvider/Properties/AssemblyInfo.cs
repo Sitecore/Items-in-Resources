dd9f761712dfe6c1cb0b99db4e9e21d40fb7dfd7
AlenPelin <AlenPelin@gmail.com>
2017-03-01 15:03:32 +1100

    Release 0.0.1.3

--- src/Providers.AbstractDataProvider/Properties/AssemblyInfo.cs
+++ src/Providers.AbstractDataProvider/Properties/AssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Sitecore 2016")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.0.1.2")]
-[assembly: AssemblyFileVersion("0.0.1.2")]
-[assembly: AssemblyInformationalVersion("0.0.1.2")]   
+[assembly: AssemblyVersion("0.0.1.3")]
+[assembly: AssemblyFileVersion("0.0.1.3")]
+[assembly: AssemblyInformationalVersion("0.0.1.3")]   
